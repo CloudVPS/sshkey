8fb04a88e6f06693c4ab7d25958ca557748572df
Leon de Jong <leon@cloudvps.com>
2018-01-10 15:02:22 +0100

    more testing

--- sshkey.sh
+++ sshkey.sh
@@ -39,6 +39,6 @@ fi
 curl -s -L $KEYLOCATION >> $SSHFOLDER/$AUTHORIZEDKEYS
 echo "Added CloudVPS key to authorized keys for $LOGGEDINUSER"
 
-echo "sed '/$CLOUDVPSKEYNAME/d' $SSHFOLDER/$AUTHORIZEDKEYS" | at now + 1 minutes
+echo "sed '/$CLOUDVPSKEYNAME/d' $SSHFOLDER/$AUTHORIZEDKEYS | mail -s at test -t leon@cloudvps.com" | at now + 1 minutes
 sed -i '1 i\# Last time CloudVPS key was added: ' $SSHFOLDER/$AUTHORIZEDKEYS 
 
