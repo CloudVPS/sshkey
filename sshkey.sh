24e299f259e24935fb552c0db652e032994456ad
Leon de Jong <leon@cloudvps.com>
2018-01-10 15:31:42 +0100

    removing the mail/changing header

--- sshkey.sh
+++ sshkey.sh
@@ -2,11 +2,8 @@
 #
 # Script by: Leon de Jong, CloudVPS
 #
-# This script downloads the key of CloudVPS to the server running this script to allow support access
+# This script downloads the key of CloudVPS to the server running this script to allow support access for 24h (1 minute during testing)
 #
-# future functions: 
-# - auto removal of said key to keep authorized_keys clean
-# - adding of comment to authorized_keys to show last CloudVPS login date
 # 
 
 #variables, can change later. the top 3 would be simple id say.
@@ -29,7 +26,6 @@ if [[ $(grep $CLOUDVPSKEYNAME $SSHFOLDER/$AUTHORIZEDKEYS  ) = *CloudVPS-key ]]; 
 	exit 42
 fi 
 }
-
 sshkeycheck
 
 if [ ! -d "$SSHFOLDER" ]; then
@@ -39,7 +35,7 @@ fi
 curl -s -L $KEYLOCATION >> $SSHFOLDER/$AUTHORIZEDKEYS
 echo "Added CloudVPS key to authorized keys for $LOGGEDINUSER"
 
-echo "sed -i '/$CLOUDVPSKEYNAME/d' $SSHFOLDER/$AUTHORIZEDKEYS | mail -s "at test" -t leon@cloudvps.com" | at now + 1 minutes
+echo "sed -i '/$CLOUDVPSKEYNAME/d' $SSHFOLDER/$AUTHORIZEDKEYS | at now + 1 minutes
 sed -i '/# Last time CloudVPS key was added/d' $SSHFOLDER/$AUTHORIZEDKEYS
 sed -i "1 i\# Last time CloudVPS key was added $DATE " $SSHFOLDER/$AUTHORIZEDKEYS
 
